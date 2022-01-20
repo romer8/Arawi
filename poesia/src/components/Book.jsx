65c3490d309157a662994c712404c2ec809ce676
romer8 <gio.busrom@gmail.com>
2022-01-19 18:30:39 -0700

    changes and testing

---
+++ poesia/src/components/Book.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export const Book = () => {
+  const [poem, setPoem] = useState([])
+  const [name, setName] = useState([])
+  const [coverimg, setCoverimg] = useState([])
+  const [author, setAuthor] = useState([])
+  const [description, setDescription] = useState([])
+
+  return <div></div>;
+};
--- poesia/src/components/Content.jsx
+++ poesia/src/components/Content.jsx
@@ -1,4 +1,54 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Content = () => {
+    const [books, setBook] = useState([])
+
+    useEffect(() => {
+        console.log(process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN);
+
+        const getBooks =  async () => {
+            const response = await axios({
+                method:"get",
+                url: 'https://api.github.com/repos/romer8/Arawi/contents/backend/Books/sin_protocolo/d1.rst',
+                headers: {
+                        Authorization: `bearer ${
+                        process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+                        }`,
+                }
+            });
+            console.log(response.data.content);
+            // return data
+         setBook("hola")
+
+      };
+      getBooks();
+    }, []);
+    
+
+    
+    // const getBooks =  async () => {
+    //     const bookFromGitHub = await fetchGithubData ()
+    //     setBook("hola")
+    //   };
+    //   getBooks();
+    // }, []);
+    
+
+    // const fetchGithubData = async () =>{
+    //     const response = await axios({
+    //         method:"get",
+    //         url: 'https://api.github.com/repos/romer8/Arawi',
+    //         headers: {
+    //                 Authorization: `bearer ${
+    //                 process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    //                 }`,
+    //         }
+    //     });
+    //     console.log(response.data);
+    //     // return data
+
+    // } 
     return (
         <div>
             <h1>hola</h1>
