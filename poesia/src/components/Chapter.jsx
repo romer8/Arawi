a21959314dda515c6a4a2f0ff246f63a709b7ccd
romer8 <gio.busrom@gmail.com>
2022-03-16 23:18:05 -0600

    extra additions to have the social media buttons on the poem instead

--- poesia/src/components/Chapter.jsx
+++ poesia/src/components/Chapter.jsx
@@ -7,7 +7,8 @@ import './Chapter.css'
 import Verses from './Verses';
 import { IoIosArrowBack } from 'react-icons/io';
 import { useNavigate } from 'react-router-dom';
-
+import { FaWhatsapp, FaQrcode } from 'react-icons/fa';
+import { FiFacebook } from 'react-icons/fi';
 const Chapter = () => {
     const navigate = useNavigate();
 
@@ -40,11 +41,18 @@ const Chapter = () => {
     }, []);
   return (
       
+    // siempre al medio la barra
     <div className='h-full relative'>
-        {/* <div className='absolute opacity-75	flex flex-row w-1/2 items-center bg-slate-900 h-full w-full lg:w-min lg:opacity-100'>
-            <div className="flex-auto bg-neutral-900 w-12 h-full text-slate-50"></div>
-            <div className="flex-auto bg-[#3A506B] w-12 h-full text-sm text-slate-50"></div>
-        </div> */}
+        <div className='fixed opacity-75 flex flex-row w-full items-center bg-slate-900 h-1/2 w-full top-56 left-2 rounded-md lg:w-min lg:opacity-100'>
+            <div className="flex flex-col bg-neutral-900 w-12 h-full text-slate-50 space-y-2">
+                <FaWhatsapp className="h-6 w-6"/>
+                <FiFacebook className="h-6 w-6"/>
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                </svg>
+            </div>
+        </div>
         <div className = 'flex flex-col h-full text-sm items-center'>
             <div className = 'mt-4 bg-zinc-900 flex flex-row h-full text-sm items-center w-full lg:bg-white' >
                 <div className='w-full font-bold text-white p-2 cursor-pointer'>  
