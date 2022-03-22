c313c3a0105d5f1afbf14b19aee79dc6ab25dbda
romer8 <gio.busrom@gmail.com>
2022-03-21 23:08:36 -0600

    Book created with animation

--- poesia/src/components/Book.jsx
+++ poesia/src/components/Book.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
+import './Book.css'
+
 const Book = ( { book } ) => {
 
   return (
     
-    <div className='flex flex-col justify-center items-center bg-white h-screen font-mono py-40'>
-      <Link to={`/book/${book.code}`} className='h-full rounded mb-20 shadow'>  
-        <img src={book.coverimg} alt={book.name} className='h-full rounded mb-20 shadow' />
-      </Link>
+    <div className='flex flex-col justify-center items-center bg-white h-full font-mono py-20'>
+      <div className='flex justify-center items-center h-full w-full book'>
+          <div className="back"></div>
+          <div className="page6"></div>
+          <div className="page5"></div>
+          <div className="page4"></div>
+          <div className="page3"></div>
+          <div className="page2"></div>
+          <div className="page1"></div>
+          <div className="front">
+            <Link to={`/book/${book.code}`} className='h-full rounded mb-20 shadow'> 
+              <img src={book.coverimg} alt={book.name} className='h-full w-full rounded mb-20 shadow ' />
+           </Link>
+          </div>
+
+      </div>
 
 
         <div className='flex flex-col justify-center items-center'>
