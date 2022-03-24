5eb7511e75112d4fdd650bddc316042b32c3ec96
romer8 <gio.busrom@gmail.com>
2022-03-23 23:05:57 -0600

    changes in the book style

--- poesia/src/components/Books.jsx
+++ poesia/src/components/Books.jsx
@@ -3,11 +3,11 @@ import Book from './Book'
 
 const Books = ({ books }) => {
   return (
-    <>
+    <div className='w-screen h-screen flex flex-row justify-center items-center flex-wrap'>
       {books.map((book, index) => (
         <Book key={index} book={book} />
       ))}
-    </>
+    </div>
   )
 }
 
