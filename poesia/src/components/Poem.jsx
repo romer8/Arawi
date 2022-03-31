44811a82cbb0a08c9a5d839c1ffe43dbeb23cf51
romer8 <gio.busrom@gmail.com>
2022-03-30 22:39:55 -0600

    added placeholder for img and description. QR was deleted.

--- poesia/src/components/Poem.jsx
+++ poesia/src/components/Poem.jsx
@@ -4,14 +4,15 @@ import QRCode from "react-qr-code";
 
 import { romanize } from 'romans';
 import { useState } from 'react';
-import { FaWhatsapp, FaQrcode } from 'react-icons/fa';
-import { FiFacebook } from 'react-icons/fi';
+import { GiRead } from 'react-icons/gi';
+import { BiMessageSquareDetail } from 'react-icons/bi';
+import { TiArrowBack } from 'react-icons/ti';
+
 
 
 const Poem = ( { poem, index}) => {
     const location = useLocation();
     const [isFlipped, setIsFlipped] = useState(false);
-
   
     const defineFlip = () => {
       setIsFlipped(!isFlipped);
@@ -20,18 +21,34 @@ const Poem = ( { poem, index}) => {
     return(
         <div className='relative'>
             <div className= {isFlipped ? "flipped scale-x-[-1] duration-700" : "scale-x-[1] duration-700"} >
-                <div className= {`flex flex-col justify-center items-center bg-white ${(index % 2 > 0) ? 'bg-stone-900' : 'bg-stone-700'} font-mono square sm:hs-auto sm:text-sm`} >
-                    <div className='flex flex-col justify-center items-center text-white  w-full h-full sm:text-sm '>
-                        { isFlipped ? 
-                            <QRCode value={`${location.pathname}/${poem.name}`} onClick={defineFlip} />
+                <div className= {`flex flex-col justify-center items-center bg-white ${(index % 2 > 0) ? 'bg-stone-900' : 'bg-stone-700'} font-mono square rounded-lg sm:hs-auto sm:text-sm`} >
+                    <div className='flex flex-col justify-center items-center text-white p-3 w-full h-full sm:text-sm '>
+                        { isFlipped ?
+                            <div className='flex flex-col justify-center items-center text-white space-y-4 w-full scale-x-[-1]' onClick={defineFlip}>
+                               <div className='flex pt-px cursor-pointer'>
+                                    <span className='text-5xl' >{romanize(index+1)}</span>
+                                    <span className='text-lg flex justify-end' >{poem.name.split('.')[0]}</span>
+                                </div>
+                                <p className='p-3 text-center'>With this utility you generate a 16 character output based on your input of numbers and upper 
+                                    and lower case letters.  Random strings can be unique. Used in computing, a random string generator 
+                                    can also be called a random character string generator. This is an important tool if you want to 
+                                    generate a unique set of strings. The utility generates a sequence that lacks a pattern and is random.</p>
+                                <div className='flex pt-px'>
+                                    <Link to={`${location.pathname}/${poem.name}`} >
+                                        <GiRead className="h-12 w-12"/>
+                                    </Link>
+                                    <TiArrowBack className="h-12 w-12" onClick={defineFlip}/>
+                                </div>
+                            </div>                        
                             :
                             <div className='flex flex-col justify-center items-center text-white space-y-4 w-full h-full'>
-                                <Link to={`${location.pathname}/${poem.name}`} className='flex flex-col justify-center items-center text-white space-y-4 w-full'>  
-                                    <h2 className='text-5xl' >{romanize(index+1)}</h2>
-                                    <h2 className='text-lg' >{poem.name.split('.')[0]}</h2>
-                                </Link>
-                                <div className='flex pt-px'>
-                                    <FaQrcode className="h-6 w-6" onClick={defineFlip}/>
+
+                                <img src="https://raw.githubusercontent.com/libgit2/libgit2sharp/master/square-logo.png" alt="" />
+                                <div className='flex pt-px cursor-pointer'>
+                                    <Link to={`${location.pathname}/${poem.name}`} >
+                                        <GiRead className="h-12 w-12"/>
+                                    </Link> 
+                                    <BiMessageSquareDetail className="h-12 w-12" onClick={defineFlip}/>
                                 </div>
                             </div>
 
